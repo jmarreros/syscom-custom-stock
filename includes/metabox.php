accbd9f2ed08f35dd96f88a64b3d6ce23b5ede59
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2023-02-15 09:39:48 -0500

    Correccion bug grabado de productos

--- includes/metabox.php
+++ includes/metabox.php
@@ -8,7 +8,7 @@ namespace dcms\syscom\stock\includes;
 class Metabox {
 	public function __construct() {
 		add_action( 'add_meta_boxes', [ $this, 'add_metabox_stock_product' ] );
-		add_action( 'save_post_product', [ $this, 'save_metabox_stock_product' ], 20, 2 );
+		add_action( 'woocommerce_update_product', [ $this, 'save_metabox_stock_product' ], 10, 2 );
 	}
 
 	public function add_metabox_stock_product(): void {
@@ -37,18 +37,17 @@ class Metabox {
 		<?php
 	}
 
-	public function save_metabox_stock_product( $product_id_woo, $post ) {
+	public function save_metabox_stock_product( $product_id_woo, $product ) {
 		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 			return;
 		}
 
-		$custom_stock = intval($_POST['custom-stock'] ?? 0);
-        update_post_meta( $product_id_woo, SYSCOM_CUSTOM_STOCK_PRODUCT, $custom_stock );
+		$custom_stock = intval( $_POST['custom-stock'] ?? 0 );
+		update_post_meta( $product_id_woo, SYSCOM_CUSTOM_STOCK_PRODUCT, $custom_stock );
 
-        //Recalculate stock
-        $stock_api = intval( get_post_meta( $product_id_woo, SYSCOM_API_STOCK_PRODUCT, true ) );
-        wc_update_product_stock( $product_id_woo, $stock_api + $custom_stock );
-
+		//Recalculate stock
+		$stock_api = intval( get_post_meta( $product_id_woo, SYSCOM_API_STOCK_PRODUCT, true ) );
+		wc_update_product_stock( $product_id_woo, $stock_api + $custom_stock, 'set', true );
 	}
 
 }
